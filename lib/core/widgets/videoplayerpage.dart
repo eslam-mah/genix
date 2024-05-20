2836a8bc6cf43869447ba2d74efa06e11e4472cf
ese_mah <eslamxxxxx517@gmail.com>
2024-05-20 22:04:17 +0300

    canceling videos temporarly

--- lib/core/widgets/videoplayerpage.dart
+++ lib/core/widgets/videoplayerpage.dart
@@ -1,39 +1,39 @@
-import 'package:appinio_video_player/appinio_video_player.dart';
-import 'package:flutter/material.dart';
+// import 'package:appinio_video_player/appinio_video_player.dart';
+// import 'package:flutter/material.dart';
 
-class VideoPlayerWidget extends StatefulWidget {
-  const VideoPlayerWidget({super.key});
+// class VideoPlayerWidget extends StatefulWidget {
+//   const VideoPlayerWidget({super.key});
 
-  @override
-  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
-}
+//   @override
+//   State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+// }
 
-class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  CustomVideoPlayerController? customVideoPlayerController;
+// class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+//   CustomVideoPlayerController? customVideoPlayerController;
 
-  @override
-  void initState() {
-    super.initState();
-    initializeVideoPlayer();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     initializeVideoPlayer();
+//   }
 
-  void initializeVideoPlayer() async {
-    var videoPlayerController = CachedVideoPlayerController.network(
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
-    await videoPlayerController.initialize();
-    setState(() {
-      customVideoPlayerController = CustomVideoPlayerController(
-          context: context, videoPlayerController: videoPlayerController);
-    });
-  }
+//   void initializeVideoPlayer() async {
+//     var videoPlayerController = CachedVideoPlayerController.network(
+//         "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+//     await videoPlayerController.initialize();
+//     setState(() {
+//       customVideoPlayerController = CustomVideoPlayerController(
+//           context: context, videoPlayerController: videoPlayerController);
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return customVideoPlayerController == null
-        ? const CircularProgressIndicator(
-            color: Colors.green,
-          )
-        : CustomVideoPlayer(
-            customVideoPlayerController: customVideoPlayerController!);
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return customVideoPlayerController == null
+//         ? const CircularProgressIndicator(
+//             color: Colors.green,
+//           )
+//         : CustomVideoPlayer(
+//             customVideoPlayerController: customVideoPlayerController!);
+//   }
+// }
